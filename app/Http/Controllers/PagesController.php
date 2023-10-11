106f8e49930b095ba75f6cca28282ea4030d06f0
Wellington-Nunes <wellingtonnunez10@gmail.com>
2023-10-11 18:38:48 -0300

    Frist commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PagesController extends Controller
+{
+    public function __construct()
+    {
+    }
+    public function index ()
+    {
+        return Inertia::render('Pages/Home', []);
+    }
+}
---
+++ resources/js/Pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React, { } from "react";
+
+export default function Home({  }) {
+
+    return (
+        <div>
+            <h1>Hello Inertia!!!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis ex quasi placeat impedit earum non corrupti pariatur saepe eligendi explicabo numquam, fuga modi animi ratione cumque blanditiis atque? Neque, suscipit?</p>
+        </div>
+    )
+}
